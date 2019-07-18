8b2af1fa29aa24bfb402afa6c4b5c0ccf562ce79
DmooSoCool <DmooSoCool@gmail.com>
2019-07-18 17:46:44 +0800

    add docker-compose.yml post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostController } from './modules/post/post.controller';
+import { PostService } from './modules/post/post.service';
+import { PostModule } from './modules/post/post.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(),
+    PostModule,
+  ],
+  controllers: [AppController, PostController],
+  providers: [AppService, PostService],
 })
 export class AppModule {}
